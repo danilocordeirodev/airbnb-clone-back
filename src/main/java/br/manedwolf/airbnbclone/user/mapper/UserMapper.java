370623ea85836dff82ce7d0136eb6e845ac85348
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-07-26 21:56:25 -0300

    add auth config

--- src/main/java/br/manedwolf/airbnbclone/user/mapper/UserMapper.java
+++ src/main/java/br/manedwolf/airbnbclone/user/mapper/UserMapper.java
@@ -1,7 +1,16 @@
 package br.manedwolf.airbnbclone.user.mapper;
 
+import br.manedwolf.airbnbclone.user.application.dto.ReadUserDTO;
+import br.manedwolf.airbnbclone.user.domain.Authority;
+import br.manedwolf.airbnbclone.user.domain.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    ReadUserDTO readUserDTOToUser(User user);
+
+    default String mapAuthoritiesToString(Authority authority) {
+        return authority.getName();
+    }
 }
